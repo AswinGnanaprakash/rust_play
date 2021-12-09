47b4d03e6461faaabdf1ec2f4fa1332cc9c6c279
AswinG <aswin@chadura.com>
2021-12-09 12:53:35 +0530

    match case practice code added, compiled successfully

---
+++ single_snippets/match_case.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let mut input_text = String::new();
+    // get the input_text from cli
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Input not given");
+    
+    let trimmed_text = input_text.trim();
+    match trimmed_text.parse::<i32>() {
+        Ok(i) => function_call(i),
+        Err(..) => println!("A bad day for you!!!"),
+    }
+}
+
+fn function_call(get_value : i32) {
+
+    match get_value { 
+        0 => println!("Empty"),
+        _ => println!("Rest of the error!!!"),
+    }
+
+}
